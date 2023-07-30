9300f54f531602b3f7592cc42a92ee104066f312
juanDiesmers <juandiegopala_@outlook.es>
2023-07-29 21:43:28 -0500

    example of funcion

--- Game/risk.cxx
+++ Game/risk.cxx
@@ -1,1 +1,13 @@
 #include "risk.h"
+#include <iostream>
+
+using namespace std;
+//Implementacion de la funcion para cargar los datos y relizar operaciones
+void cargarPaises(std::vector<Pais>& paises){
+    std::cout << "Inicializando comando cargarPaises....\n";
+
+    std::cout << "cargando Pasies...\n";
+
+    std::cout << "Funcion terminada...\n";
+
+}
