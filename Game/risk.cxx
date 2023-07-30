943ad9c2948817a2c1426ec120bf8edd82dcd9d9
Jairo Andrés <133978172+Jairo-Andres@users.noreply.github.com>
2023-07-29 22:23:26 -0500

    IntentandoXD

--- Game/risk.cxx
+++ Game/risk.cxx
@@ -10,11 +10,85 @@ void cargarPaises(std::vector<Pais>& paises){
 
     std::cout << "Funcion terminada...\n";
 
-
 }
 
 void Jugador::conquistaPais(Pais& pais, int ejercitos) {
     std::cout << "Ingreso correctamente a la funcion conquisto pais." << std::endl;
 
+}
+
+void Jugador::atacarPais(Pais& pais, int ejercitos){
+    std::cout << "Ingreso correctamente a la funcion Atacar Pais." << std::endl;
+
+}
+
+void Jugador::reagruparEjercitos(Pais& origen, Pais& destino, int ejercitos){
+    std::cout << "Ingreso correctamente a reagrupar Ejercitos." << std::endl;
+
+}
+
+void cargarContienentes(std::vector<Continente>& continentes){
+    std::cout <<"Ingreso correctamente a Cargar Contienentes. " << std::endl;
+
+}
+
+void cargarFronteras(std::map<std::string, std::vector<std::string>>& fronteras){
+    std:: cout <<"Ingreso correctamente a Cargar Fronteras. " << std::endl;
+    
+}
+//
+void asignarObjetivosAleatorios(std::vector<Jugador>& jugadores) {
+    std::cout << "Ingreso correctamente a la función asignar Objetivos Aleatorios." << std::endl;
+    
+}
+
+void asignarObjetivosPorPaises(std::vector<Jugador>& jugadores) {
+    std::cout << "Ingreso correctamente a la función asignar Objetivos PorPaises." << std::endl;
+    
+}
+
+void asignarObjetivosPorContinente(std::vector<Jugador>& jugadores) {
+    std::cout << "Ingreso correctamente a la función asignar Objetivos Por Continente." << std::endl;
+    
+}
+
+void asignarObjetivosPorJugador(Jugador& jugador) {
+    std::cout << "Ingreso correctamente a la función asignar Objetivos Por Jugador." << std::endl;
+    
+}
+
+void mostrarObjetivos(const std::vector<Jugador>& jugadores) {
+    std::cout << "Ingreso correctamente a la función mostrar Objetivos." << std::endl;
+    
+}
+
+void mostrarObjetivosJugador(const Jugador& jugador) {
+    std::cout << "Ingreso correctamente a la función mostrar Objetivos del Jugador." << std::endl;
+    
+}
+
+void mostrarPaises(const std::vector<Pais>& paises) {
+    std::cout << "Ingreso correctamente a la función mostrar Paises." << std::endl;
+    
+}
+
+void mostrarContinente(const std::vector<Continente>& continentes) {
+    std::cout << "Ingreso correctamente a la función mostrar Continente." << std::endl;
+    
+}
+
+void mostrarFronteras(const std::map<std::string, std::vector<std::string>>& fronteras) {
+    std::cout << "Ingreso correctamente a la función mostrar Fronteras." << std::endl;
+    
+}
+
+void mostrarConquista(const std::string& nombreTerritorio) {
+    std::cout << "Ingreso correctamente a la función mostrar Conquista." << std::endl;
+    
+}
+
+void mostrarAyuda() {
+    std::cout << "Ingreso correctamente a la función mostrar Ayuda." << std::endl;
+    
 }
 
