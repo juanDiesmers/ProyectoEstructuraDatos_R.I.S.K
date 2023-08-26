3efbb6a4733559043fdcb8308fc467b959ba54c8
amers_gos <juandiegopala_@outlook.es>
2023-08-26 18:14:43 -0500

    functions of the turno file

--- src/Turno/turno.h
+++ src/Turno/turno.h
@@ -1,6 +1,9 @@
 #ifndef TURNO_H
 #define TURNO_H
 
-
-
+//Funcion de Turno del jugador
+void turnoJugador(std::vector<Jugador>& jugadores, std::vector<Territorio>& territorios);
+//Salir del juego
+void salir();
+//Validacion del jugador
 #endif //TURNO_H
